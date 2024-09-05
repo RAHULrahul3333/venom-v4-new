016c8af38bf587240731d0d0895656afa1c0a54e
RAHULrahul3333 <rahulpatel19438@gmail.com>
2024-09-05 22:32:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6842926603:AAHXW85NGUPzlpIY1n_f7xrKcyWaguMRyAM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Forkner_2233_bot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("937311092")
 
 
 
